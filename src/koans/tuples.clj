5de8bc2f837890bbb2d9ca61e4625d94cd7be1e2
Sam Ritchie <sritchie09@gmail.com>
2011-11-20 16:47:49 -0500

    First thoughts in interface.

--- src/koans/tuples.clj
+++ src/koans/tuples.clj
@@ -4,5 +4,20 @@
  "We shall contemplate truth by testing reality, via equality."
  (= __ [["truth."]])
 
- "To understand reality, we must compare our expectations against reality."
- (?= __ (<- [?x] (one-tuple ?x))))
+ "Queries produce results..."
+ (?= __ (<- [?x] (one-tuple ?x)))
+
+ "All sorts of sequences do the job."
+ (?= __ (<- [?x] ([[1]] ?x)))
+
+ "Lists, anyone?"
+ (?= __ (<- [?x] ((list [1]) ?x)))
+
+ "From whence do these tuples come?"
+ (?= [[4]] (<- [?x] (__ ?x)))
+
+ "Name something well to give it power."
+ (?= one-tuple (<- [?x] (one-tuple __)))
+
+ "The name itself doesn't matter, just that it match."
+ (?= one-tuple (<- [__] (one-tuple __))))
