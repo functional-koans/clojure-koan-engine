201e6aed9b00af863e243872a4dde862bfc08235
David Kinzer <dtkinzer@gmail.com>
2013-07-29 03:44:54 +0000

    Add evaluation errors to koan message.
    This commit attempts to implement the second option proposed in the
    discussion to issue #6:
    
    This commit adds the evaluation errors that occur in the course
    of testing a koan to the default koan message.
    
    These errors are not added if the user has not completed the koan (i.e. there
    are still "__" in the koan).
    
    Below is an example of the type of output this change introduces:
    
    Now meditate upon /home/vagrant/clojure-koans/src/koans/17_macros.clj:42
    ---------------------
    Assertion failed!
    Really, you don't understand recursion until you understand recursion
    (= 36 (r-infix (10 + (2 * 3) + (4 * 5))))
    
    -------------
    Part-2 Error: java.lang.Long cannot be cast to clojure.lang.IFn

--- src/koan_engine/util.clj
+++ src/koan_engine/util.clj
@@ -1,4 +1,5 @@
 (ns koan-engine.util
+  (::use [clojure.string :only  [split]])
   (:require [clojure.string :as s]
             [clojure.java.io :as io])
   (:import [java.net URLDecoder]))
@@ -29,6 +30,55 @@
        `(assert ~x)
        `(assert ~x ~msg))))
 
+(defn answered?
+  "Returns true if an attempt has been made to answer the koan
+  or false otherwise."
+  [form] (not (contains? 
+          (split (str form) #"\s+") "__")))
+
+(defn get-evaluation-errors
+  "Iterates over the the part of an equality form and evaluates each in turn.
+  Returns a list of evaluation errors if any; an empty list otherwise."
+  ([form] (get-evaluation-errors form 1 []))
+  ([form n errors] 
+   (if (= n (count form))
+     errors
+     ; Cannot use loop/recur form here because recur is not allowed
+     ; across the try/catch form.
+     (try 
+       (eval (nth form n))
+       (get-evaluation-errors form (inc n) errors)
+       (catch Throwable e#
+         (get-evaluation-errors form (inc n)
+                            (conj errors (str "Part-" n " Error: " (.getMessage e#)))))))))
+
+(defn print-errors-string
+  "Given the results of get-evaluation-errors[] returns as formatted string."
+  [results] (if (= 0 (count results))
+              ""
+              (str
+                "\n-------------\n"
+                (loop [n 0
+                       results results
+                       s ""]
+                  (if (= n (count results))
+                    s
+                    (recur (inc n) results (str s (nth results n "No errors."))))))))
+
+
+(defn get-evaluation-errors-proxy
+  "Given a quoted equality form, retrieves any evaluation errors
+  in the different parts of the form, and returns them as a string. 
+  May return an empty string."
+  [form] (if
+           (and 
+             (answered? form)
+             ; Protect against a future failure if a koan uses something
+             ; other than the equality form for the assertion.
+             (= "=" (str (first form))))
+           (print-errors-string (get-evaluation-errors form)) 
+           ""))
+
 (defmacro fancy-assert
   "Assertion with fancy error messaging."
   ([x] (fancy-assert x ""))
@@ -37,7 +87,10 @@
            (catch Throwable e#
              (throw (Exception. (str ~(when-let [line (:line (meta x))]
                                         (str "[LINE " line "] "))
-                                     '~message "\n" '~x)))))))
+                                     '~message "\n" '~x "\n"
+                                     (get-evaluation-errors-proxy '~x))))))))
+
+
 
 (defn read-project []
   (let [rdr (clojure.lang.LineNumberingPushbackReader.
