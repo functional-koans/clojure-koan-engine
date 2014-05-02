6cd172b6671a2ce671876840c4fbdb51d906e8b9
Colin Jones <trptcolin@gmail.com>
2014-05-02 16:35:02 -0500

    Bump to 0.2.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject koan-engine "0.2.1"
+(defproject koan-engine "0.2.2"
   :description "Koan Engine for Clojure projects."
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [fresh "1.0.2"]
