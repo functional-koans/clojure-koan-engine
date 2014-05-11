aa25141cc377b79678dddfb2be718da91924d019
Benedek Fazekas <benedek.fazekas@gmail.com>
2014-05-11 17:37:09 +0100

    adds random runner
    - with `lein koan random` a random koan selected to be solved (needs a
      small modification of the lein plugin)
    - on success next random koan is selected
    - on failure the same koan presented again
    - there is an option to skip-koan if the padawan want to proceed to the
      next koan
    - if multiple placeholders are present in the koan, multiple solutions
      can be typed in with ';' as delimiter
    - the context of the koan is presented and considered for
      placeholders (typically when the koan file contains not fully
      implemented functions, macros)

--- src/koan_engine/runner.clj
+++ src/koan_engine/runner.clj
@@ -1,7 +1,8 @@
 (ns koan-engine.runner
   (:use [koan-engine.freshness :only [setup-freshener]])
   (:require [koan-engine.util :as u]
-            [koan-engine.checker :as checker]))
+            [koan-engine.checker :as checker]
+            [koan-engine.random :as random]))
 
 (def default-koan-map
   {:koan-root "src/koans"
@@ -16,4 +17,5 @@
                         (:koan (u/read-project)))]
     (case task
       "run"  (setup-freshener koan-map)
-      "test" (checker/test koan-map))))
+      "test" (checker/test koan-map)
+      "random" (random/runner koan-map))))
