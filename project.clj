22176fd536dd9842be7dcf342a9dae3e2316736c
Colin Jones <trptcolin@gmail.com>
2017-10-24 11:07:39 -0500

    Remove jline & repl scripts

--- project.clj
+++ project.clj
@@ -1,5 +1,4 @@
-(defproject koan-engine "0.2.3"
+(defproject koan-engine "0.2.4"
   :description "Koan Engine for Clojure projects."
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [fresh "1.0.2"]
-                 [jline "0.9.94" :exclusions [junit]]])
+                 [fresh "1.0.2"]])
